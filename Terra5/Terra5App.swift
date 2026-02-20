671adff3801bf1cef287538adcafd83b0a9dcf1e
portwally <walter.tengler@gmail.com>
2026-02-20 13:07:37 +0000

    Initial Commit

---
+++ Terra5/Terra5App.swift
@@ -0,0 +1,17 @@
+//
+//  Terra5App.swift
+//  Terra5
+//
+//  Created by Walter Tengler on 20/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Terra5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
